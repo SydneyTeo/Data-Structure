6cdd9bc4dca8317fb207b4e4754fc888dd8fcc3c
SydneyTeo <sydn0002@e.ntu.edu.sg>
2020-12-17 21:37:12 +0800

    3.1

--- week2/week3/3.1moneychange.py
+++ week2/week3/3.1moneychange.py
@@ -1,13 +1,14 @@
 # Uses python3
 import sys
+import math
 
-def get_change(amt):
+def get_change(m):
     #write your code here
     coins = [10, 5, 1]  # must be sorted
     count = 0
     for coin in coins:
-        count += amt // coin
-        amt %= coin
+        count += m // coin
+        m %= coin
     return count
 
 if __name__ == '__main__':
