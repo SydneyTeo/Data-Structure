6f05d9633c9c7342c6c2121fbe01c85fe935d307
SydneyTeo <sydn0002@e.ntu.edu.sg>
2020-12-17 21:27:38 +0800

    3.1

--- week2/week3/3.1moneychange.py
+++ week2/week3/3.1moneychange.py
@@ -1,18 +1,15 @@
 # Uses python3
-
+import sys
 
 def get_change(amt):
     #write your code here
     coins = [10, 5, 1]  # must be sorted
     count = 0
     for coin in coins:
-        # Update count with the the number of coins 'are held' in the amount.
         count += amt // coin
-        # Put remainder to the residuary amount.
-        amt %= coins
-
+        amt %= coin
     return count
 
 if __name__ == '__main__':
-    m = int(input())
+    m = int(sys.stdin.read())
     print(get_change(m))
